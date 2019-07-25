ef3dc16a401b989f70ab29eac77e5b64ad8426c2
MR Julien Lubeek <45044627@students.mq.edu.au>
2019-07-25 16:17:31 +1000

    More subsetting

--- Scripts/Introduction.R
+++ Scripts/Introduction.R
@@ -41,6 +41,16 @@ tricky <- c (1, 2, 3, "4")
 
 combined_logical <- c(num_logical, char_logical)
 
+#Subseting
+
+respondent_wall_type[2]
+respondent_wall_type[c(3,2)]
+
+more_respondent_wall_type <- respondent_wall_type[c(1,2,3,2,1,3)]
+
+
+no_membrs[c(TRUE, FALSE, TRUE, TRUE)]
+no_membrs[no_membrs > 5]
 
 
 
