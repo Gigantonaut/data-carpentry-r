a92c9ca02e71ffd50a28c9e4be6be26cb516098f
MR Julien Lubeek <45044627@students.mq.edu.au>
2019-07-25 15:37:54 +1000

    Assign areas to area_hectares

--- Scripts/Introduction.R
+++ Scripts/Introduction.R
@@ -3,3 +3,13 @@
 library (tidyverse)
 
 print ("Hello, world")
+
+(area_hectares <- 1.0)
+1.0 -> area_hectares
+
+area_hectares <- 2.5                 # land area in hectares
+area_acres <- 2.47 * area_hectares   # convert to acres
+area_acres                           # print 
+
+
+
