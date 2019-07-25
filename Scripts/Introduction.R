496f8c0a107cbbd74c26ac11bc020215cabd08b2
MR Julien Lubeek <45044627@students.mq.edu.au>
2019-07-25 15:40:35 +1000

    Assign areas to area_hectares

--- Scripts/Introduction.R
+++ Scripts/Introduction.R
@@ -11,5 +11,7 @@ area_hectares <- 2.5                 # land area in hectares
 area_acres <- 2.47 * area_hectares   # convert to acres
 area_acres                           # print 
 
-
-
+length <- 5
+width <- 3.2
+area <- length * width
+area
