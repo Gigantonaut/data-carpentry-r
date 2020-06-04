39e6124f11ae41f0075c5e13487d43a6f44314f5
Gigantonaut <53256411+Gigantonaut@users.noreply.github.com>
2020-06-04 20:14:40 +1000

    Add files via upload

---
+++ Scripts/gigantowear2manova.R
@@ -0,0 +1,7 @@
+# giganto.microwear2
+# Perform MANOVA on 5 Giganto bearing sites (exl. Bapeng, molar to be scanned)
+giganto.microwear2
+giganto.manova1
+names(giganto.microwear2)
+summary(giganto.manova1)
+
